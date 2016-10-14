21dff8e31e58f79109c4d07ec005bafa202d6e05
Grimmer Kang <grimmer0125@gmail.com>
2016-10-14 16:49:16 +0800

    commit missing code.

--- update.js
+++ update.js
@@ -1,6 +1,15 @@
 var fetcher = require("./fetcher.js")
+var fs = require('fs');
 
-fetcher.loadORDownload(function(housePrice){
+fetcher.loadORDownload(function(housePrice, newCityList){
 
   console.log("fetch result:", housePrice);
+  var writeData = "var housePrice ="+JSON.stringify(newCityList)+";"
+  fs.writeFile("./data/price.js", writeData , function(err) {
+      if(err) {
+          return console.log(err);
+      }
+
+      console.log("The file was saved!");
+  });
 });
